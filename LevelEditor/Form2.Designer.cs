ac03cea8cd37c0e66eb7387c7440bd37a0650e71
Xanonblade <116854916+Xanonblade@users.noreply.github.com>
2026-02-25 13:45:42 -0500

    Added level editor to project

---
+++ LevelEditor/Form2.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace HW2_LevelEditor
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            tileGray = new PictureBox();
+            tileBrown = new PictureBox();
+            tileBlue = new PictureBox();
+            tileGreen = new PictureBox();
+            tileTan = new PictureBox();
+            tileRed = new PictureBox();
+            groupBox2 = new GroupBox();
+            tileCurrent = new PictureBox();
+            buttonSave = new Button();
+            buttonLoad = new Button();
+            groupBoxMap = new GroupBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)tileGray).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tileBrown).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tileBlue).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tileGreen).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tileTan).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tileRed).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)tileCurrent).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tileGray);
+            groupBox1.Controls.Add(tileBrown);
+            groupBox1.Controls.Add(tileBlue);
+            groupBox1.Controls.Add(tileGreen);
+            groupBox1.Controls.Add(tileTan);
+            groupBox1.Controls.Add(tileRed);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(153, 237);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tile Selector";
+            // 
+            // tileGray
+            // 
+            tileGray.BackColor = Color.Gray;
+            tileGray.Location = new Point(79, 163);
+            tileGray.Name = "tileGray";
+            tileGray.Size = new Size(62, 62);
+            tileGray.TabIndex = 5;
+            tileGray.TabStop = false;
+            tileGray.Click += TileSelect_Click;
+            // 
+            // tileBrown
+            // 
+            tileBrown.BackColor = Color.Sienna;
+            tileBrown.Location = new Point(79, 95);
+            tileBrown.Name = "tileBrown";
+            tileBrown.Size = new Size(62, 62);
+            tileBrown.TabIndex = 4;
+            tileBrown.TabStop = false;
+            tileBrown.Click += TileSelect_Click;
+            // 
+            // tileBlue
+            // 
+            tileBlue.BackColor = Color.SteelBlue;
+            tileBlue.Location = new Point(79, 27);
+            tileBlue.Name = "tileBlue";
+            tileBlue.Size = new Size(62, 62);
+            tileBlue.TabIndex = 3;
+            tileBlue.TabStop = false;
+            tileBlue.Click += TileSelect_Click;
+            // 
+            // tileGreen
+            // 
+            tileGreen.BackColor = Color.YellowGreen;
+            tileGreen.Location = new Point(11, 163);
+            tileGreen.Name = "tileGreen";
+            tileGreen.Size = new Size(62, 62);
+            tileGreen.TabIndex = 2;
+            tileGreen.TabStop = false;
+            tileGreen.Click += TileSelect_Click;
+            // 
+            // tileTan
+            // 
+            tileTan.BackColor = Color.Khaki;
+            tileTan.Location = new Point(11, 95);
+            tileTan.Name = "tileTan";
+            tileTan.Size = new Size(62, 62);
+            tileTan.TabIndex = 1;
+            tileTan.TabStop = false;
+            tileTan.Click += TileSelect_Click;
+            // 
+            // tileRed
+            // 
+            tileRed.BackColor = Color.Firebrick;
+            tileRed.Location = new Point(11, 27);
+            tileRed.Name = "tileRed";
+            tileRed.Size = new Size(62, 62);
+            tileRed.TabIndex = 0;
+            tileRed.TabStop = false;
+            tileRed.Click += TileSelect_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(tileCurrent);
+            groupBox2.Location = new Point(12, 255);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(153, 153);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Current Tile";
+            // 
+            // tileCurrent
+            // 
+            tileCurrent.Location = new Point(32, 39);
+            tileCurrent.Name = "tileCurrent";
+            tileCurrent.Size = new Size(86, 86);
+            tileCurrent.TabIndex = 6;
+            tileCurrent.TabStop = false;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(12, 436);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(153, 77);
+            buttonSave.TabIndex = 2;
+            buttonSave.Text = "Save File";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += ButtonSave_Click;
+            // 
+            // buttonLoad
+            // 
+            buttonLoad.Location = new Point(12, 519);
+            buttonLoad.Name = "buttonLoad";
+            buttonLoad.Size = new Size(153, 77);
+            buttonLoad.TabIndex = 3;
+            buttonLoad.Text = "Load File";
+            buttonLoad.UseVisualStyleBackColor = true;
+            buttonLoad.Click += ButtonLoad_Click;
+            // 
+            // groupBoxMap
+            // 
+            groupBoxMap.Location = new Point(171, 12);
+            groupBoxMap.Name = "groupBoxMap";
+            groupBoxMap.Size = new Size(651, 598);
+            groupBoxMap.TabIndex = 4;
+            groupBoxMap.TabStop = false;
+            groupBoxMap.Text = "Map";
+            // 
+            // Form2
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(835, 622);
+            Controls.Add(groupBoxMap);
+            Controls.Add(buttonLoad);
+            Controls.Add(buttonSave);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form2";
+            Text = "Level Editor";
+            FormClosing += Form2_FormClosing;
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)tileGray).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tileBrown).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tileBlue).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tileGreen).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tileTan).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tileRed).EndInit();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)tileCurrent).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button buttonSave;
+        private Button buttonLoad;
+        private GroupBox groupBoxMap;
+        private PictureBox tileGray;
+        private PictureBox tileBrown;
+        private PictureBox tileBlue;
+        private PictureBox tileGreen;
+        private PictureBox tileTan;
+        private PictureBox tileRed;
+        private PictureBox tileCurrent;
+    }
+}
---
+++ LevelEditor/Form2.cs
@@ -0,0 +1,258 @@
+﻿
+namespace HW2_LevelEditor
+{
+    public partial class Form2 : Form
+    {
+        //Fields
+        private int _width;
+        private int _height;
+        private string? _fileName;
+        private List<PictureBox>? _tileList;
+        private bool _unsavedChanges;
+
+        /// <summary>
+        /// Constructs a form for editing a completely new map
+        /// </summary>
+        /// <param name="width">Width of new map</param>
+        /// <param name="height">Height of new map</param>
+        public Form2(int width, int height)
+        {
+            this._width = width;
+            this._height = height;
+            _tileList = new List<PictureBox>();
+            _unsavedChanges = false;
+
+            InitializeComponent();
+
+            //Prompts the method that creates the grid
+            CreateMap();
+        }
+
+        /// <summary>
+        /// Constructs a form for editing an existing map
+        /// </summary>
+        /// <param name="fileName">Name of map to edit</param>
+        public Form2(string fileName)
+        {
+            this._fileName = fileName;
+            _tileList = new List<PictureBox>();
+            _unsavedChanges = false;
+
+            InitializeComponent();
+
+            //Call the loadFile method here as this constructor is only called when the user requests to load a file
+            LoadFile(fileName);
+        }
+
+        //Methods
+
+        /// <summary>
+        /// Creates and displays grid of picture tiles
+        /// </summary>
+        public void CreateMap()
+        {
+            //Dispose of all existing tiles and empty list first
+            for (int i = 0; i < _tileList!.Count; i++)
+            {
+                _tileList[i].Dispose();
+            }
+            _tileList!.Clear();
+
+            for (int i = 0; i < _width; i++)
+            {
+                for (int j = 0; j < _height; j++)
+                {
+                    //Temporary tile to place in list
+                    PictureBox tempTile = new PictureBox();
+                    tempTile.Height = 550 / _height;
+                    tempTile.Width = tempTile.Height;
+
+                    //Rescale window based on width
+                    groupBoxMap.Width = tempTile.Width * _width + 28;
+                    this.Width = 202 + groupBoxMap.Width;
+
+                    //Default colors
+                    if ((j + i) % 2 == 0)
+                    {
+                        tempTile.BackColor = Color.White;
+                    }
+                    else
+                    {
+                        tempTile.BackColor = Color.LightGray;
+                    }
+
+                    //Adding tile to groupBox and list
+                    tempTile.Click += TileMap_Click!;
+                    groupBoxMap.Controls.Add(tempTile);
+                    Point newPosition = new Point(i * tempTile.Width + 14, j * tempTile.Height + 26);
+                    tempTile.Location = newPosition;
+                    _tileList!.Add(tempTile);
+                    _tileList[j + Math.Abs(_width - Math.Abs(_width - _height)) * i].Show();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets color of clicked tile to that of the currently selected tile color
+        /// </summary>
+        private void TileMap_Click(object sender, EventArgs e)
+        {
+            PictureBox tile = (PictureBox)sender;
+
+            //Unsaved changes
+            if (_unsavedChanges == false && tileCurrent.BackColor != tile.BackColor)
+            {
+                _unsavedChanges = true;
+                this.Text += "*";
+            }
+
+            tile.BackColor = tileCurrent.BackColor;
+        }
+
+        /// <summary>
+        /// Sets the user's current tile to the one they click on
+        /// </summary>
+        private void TileSelect_Click(object sender, EventArgs e)
+        {
+            PictureBox tile = (PictureBox)sender;
+
+            tileCurrent.BackColor = tile.BackColor;
+        }
+
+        /// <summary>
+        /// Saves map by reading in width, height, and color of every tile
+        /// </summary>
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            //Create file saver
+            SaveFileDialog fileSaver = new SaveFileDialog();
+            fileSaver.Filter = "Level Files|*.level";
+            fileSaver.Title = "Save a level file";
+
+            //Get result and proceed
+            DialogResult savedFileStatus = fileSaver.ShowDialog();
+
+            if (savedFileStatus == DialogResult.OK)
+            {
+                //Save file
+                StreamWriter output = null!;
+
+                try
+                {
+                    output = new StreamWriter(fileSaver.FileName);
+
+                    //Write width and height first
+                    output.WriteLine(_width.ToString());
+                    output.WriteLine(_height.ToString());
+
+                    //Loop through list and write all colors, one per line
+                    for (int i = 0; i < _tileList!.Count; i++)
+                    {
+                        output.WriteLine(_tileList[i].BackColor.ToArgb());
+                    }
+
+                    //Success message
+                    MessageBox.Show("File saved successfully", "File saved",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Text = $"Level Editor - {fileSaver.FileName.Substring(fileSaver.FileName.LastIndexOf('\\') + 1)}";
+                    _unsavedChanges = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex}", "ERROR",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    if (output != null)
+                    {
+                        output.Close();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Loads map by getting a filename and then calling LoadFile()
+        /// </summary>
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            //Create file opener
+            OpenFileDialog fileOpener = new OpenFileDialog();
+            fileOpener.Filter = "Level Files|*.level";
+            fileOpener.Title = "Load a level file";
+
+            //Get result and proceed
+            DialogResult openedFileStatus = fileOpener.ShowDialog();
+
+            if (openedFileStatus == DialogResult.OK)
+            {
+                LoadFile(fileOpener.FileName);
+            }
+        }
+
+        /// <summary>
+        /// Loads the given file, which contains width, height, and all values to
+        /// be placed in mapGrid array
+        /// </summary>
+        /// <param name="fileName">File to load</param>
+        public void LoadFile(string fileName)
+        {
+            StreamReader input = null!;
+
+            try
+            {
+                input = new StreamReader(fileName);
+
+                //Call CreateMap() to set up map before repainting
+                _width = int.Parse(input.ReadLine()!);
+                _height = int.Parse(input.ReadLine()!);
+                CreateMap();
+
+                //Loop and paint
+                for (int i = 0; i < _tileList!.Count; i++)
+                {
+                    _tileList[i].BackColor = Color.FromArgb(int.Parse(input.ReadLine()!));
+                }
+
+                //Success message
+                MessageBox.Show("File loaded successfully", "File loaded",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Text = $"Level Editor - {fileName.Substring(fileName.LastIndexOf('\\') + 1)}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex}", "ERROR",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+            finally
+            {
+                if (input != null)
+                {
+                    input.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prompts user about saving changes before closing
+        /// </summary>
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_unsavedChanges)
+            {
+                DialogResult closeResult = MessageBox.Show("There are unsaved changes. Are you sure you want to quit?", "Unsaved changes",
+                    MessageBoxButtons.YesNo,MessageBoxIcon.Warning);
+
+                if (closeResult == DialogResult.No)
+                {
+                    //File doesn't close
+                    e.Cancel = true;
+                }
+
+                //Otherwise, file closes as normal
+            }
+        }
+    }
+}
