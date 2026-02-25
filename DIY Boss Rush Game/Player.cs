e20ff9e3b8a9c81814086b282ef9543a5635cb59
SlyCash <smc4598@g.rit.edu>
2026-02-25 13:17:16 -0500

    Created Character and Player classes

---
+++ DIY Boss Rush Game/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIY_Boss_Rush_Game
+{
+    internal class Player
+    {
+    }
+}
---
+++ DIY Boss Rush Game/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new DIY_Boss_Rush_Game.Game1();
+game.Run();
