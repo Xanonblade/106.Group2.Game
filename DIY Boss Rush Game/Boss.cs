45613a3177581a2ed3dfa0826c5e0c931c87aaf2
Corey Stinson <corey@coreystinson.com>
2026-03-01 11:07:40 -0500

    Did more work on the boss AI
    Still have to add comments

--- DIY Boss Rush Game/Boss.cs
+++ DIY Boss Rush Game/Boss.cs
@@ -6,6 +6,9 @@ using System.IO;
 
 namespace DIY_Boss_Rush_Game
 {
+    /// <summary>
+    /// All of the possible actions the boss can take
+    /// </summary>
     public enum Action
     {
         Move,
@@ -14,75 +17,123 @@ namespace DIY_Boss_Rush_Game
         Attack,
         Wait
     }
+    /// <summary>
+    /// Holds all of the boss's information and handles its AI
+    /// </summary>
     internal class Boss : Character
     {
         public static Vector2 position;
 
+        // Fields for the boss's actions
         private List<Queue<Action>> sequences;
         private Queue<Action> currentSequence;
-        private float timeForNextAction;
+        private Action currentAction;
         private bool isActionFinished;
 
+        // 
         private GameTime gameTime;
+        private Random random;
 
-        public Boss() : base()
+        private float waitTime;
+        private Vector2 selectedMovePos;
+        private Vector2 playerPos;
+
+        public Boss(Rectangle rect, Texture2D texture, int healthStat, int damageStat, int speedStat, int critStat) : 
+            base(rect, texture, healthStat, damageStat, speedStat, critStat)
         {
             sequences = new List<Queue<Action>>();
             ReadSequences("BossSequences.txt");
 
-            timeForNextAction = 0;
-            isActionFinished = false;
+            waitTime = 0;
+            isActionFinished = true;
+            random = new Random();
         }
 
         public override void Update(GameTime gameTime)
         {
+            base.Update(gameTime);
+            this.gameTime = gameTime;
+
             if (isActionFinished)
             {
                 DetermineAction();
             }
-
-            this.gameTime = gameTime;
+            DoAction();
         }
 
         public override void Draw(SpriteBatch sb)
         {
-
+            base.Draw(sb);
         }
 
         private void DetermineAction()
         {
-            if (currentSequence == null)
+            if (currentSequence == null || currentSequence.Count <= 0)
             {
-                Random random = new Random();
-
                 currentSequence = sequences[random.Next(0, sequences.Count)];
-                Action currentAction = currentSequence.Dequeue();
-
-                switch (currentAction)
-                {
-                    case Action.Move:
-                        break;
-                    case Action.Charge:
-                        break;
-                    case Action.Retreat:
-                        break;
-                    case Action.Attack:
-                        break;
-                    case Action.Wait:
-                        break;
-                }
             }
 
-            
+            currentAction = currentSequence.Dequeue();
+
+            isActionFinished = false;
+            waitTime = 0;
+
+
+            int width = 1920;
+            int height = 1080;
+            int buffer = 50;
+
+            playerPos = Player.pos;
+
+            if (currentAction == Action.Move)
+            {
+                selectedMovePos = new Vector2(random.Next(buffer, width - buffer), 
+                    random.Next(buffer, height - buffer));
+            }
+            else if (currentAction == Action.Retreat)
+            {
+                Vector2 awayDirection = Vector2.Normalize(position - playerPos);
+
+                float retreatDistance = 300f;
+                Vector2 targetRetreatPos = position + (awayDirection * retreatDistance);
+                
+                // Clamp position to stay within bounds
+                selectedMovePos = new Vector2(
+                    Math.Clamp(targetRetreatPos.X, buffer, width - buffer),
+                    Math.Clamp(targetRetreatPos.Y, buffer, height - buffer)
+                );
+            }
+        }
+
+        private void DoAction()
+        {
+            switch (currentAction)
+            {
+                case Action.Move:
+                    Move(selectedMovePos, 1);
+                    break;
+                case Action.Charge:
+                    Move(playerPos, 1.5f);
+                    break;
+                case Action.Retreat:
+                    Move(selectedMovePos, 1);
+                    break;
+                case Action.Attack:
+                    Attack();
+                    break;
+                case Action.Wait:
+                    Wait(1.5f);
+                    break;
+            }
         }
 
         private void Move(Vector2 destination, float speedMult = 1)
         {
             Vector2 direction = Vector2.Normalize(destination - position);
-            Vector2 movement = direction * speedMult /* * speedStat*/ * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Vector2 movement = direction * speedMult * SpeedStat * (float)gameTime.ElapsedGameTime.TotalSeconds;
             position += movement;
 
-            if (position == destination)
+            if (Vector2.DistanceSquared(position, destination) <= 25f)
             {
                 isActionFinished = true;
             }
@@ -93,9 +144,14 @@ namespace DIY_Boss_Rush_Game
 
         }
 
-        private void Wait()
+        private void Wait(float timeToWait)
         {
+            waitTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            if (waitTime >= 3f)
+            {
+                isActionFinished = true;
+            }
         }
 
         private void ReadSequences(string fileName)
@@ -117,6 +173,8 @@ namespace DIY_Boss_Rush_Game
                     sequences[^1].Enqueue(Action.Parse<Action>(seq[i]));
                 }
             }
+
+            sr.Close();
         }
     }
 }
